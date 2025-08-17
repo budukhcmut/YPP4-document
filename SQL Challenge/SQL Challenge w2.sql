106feabc85e0c6efccdecc8432cb240e34a0fa0c
budukhcmut <khiembuihk22@gmail.com>
2025-08-17 15:28:22 +0700

    add w2

---
+++ SQL Challenge/SQL Challenge w2.sql
@@ -0,0 +1,254 @@
+
+DROP TABLE IF EXISTS runners;
+CREATE TABLE runners (
+  "runner_id" INTEGER,
+  "registration_date" DATE
+);
+INSERT INTO runners
+  ("runner_id", "registration_date")
+VALUES
+  (1, '2021-01-01'),
+  (2, '2021-01-03'),
+  (3, '2021-01-08'),
+  (4, '2021-01-15');
+
+
+DROP TABLE IF EXISTS customer_orders;
+CREATE TABLE customer_orders (
+  "order_id" INTEGER,
+  "customer_id" INTEGER,
+  "pizza_id" INTEGER,
+  "exclusions" VARCHAR(4),
+  "extras" VARCHAR(4),
+  "order_time" TIMESTAMP
+);
+
+INSERT INTO customer_orders
+  ("order_id", "customer_id", "pizza_id", "exclusions", "extras", "order_time")
+VALUES
+  ('1', '101', '1', '', '', '2020-01-01 18:05:02'),
+  ('2', '101', '1', '', '', '2020-01-01 19:00:52'),
+  ('3', '102', '1', '', '', '2020-01-02 23:51:23'),
+  ('3', '102', '2', '', NULL, '2020-01-02 23:51:23'),
+  ('4', '103', '1', '4', '', '2020-01-04 13:23:46'),
+  ('4', '103', '1', '4', '', '2020-01-04 13:23:46'),
+  ('4', '103', '2', '4', '', '2020-01-04 13:23:46'),
+  ('5', '104', '1', 'null', '1', '2020-01-08 21:00:29'),
+  ('6', '101', '2', 'null', 'null', '2020-01-08 21:03:13'),
+  ('7', '105', '2', 'null', '1', '2020-01-08 21:20:29'),
+  ('8', '102', '1', 'null', 'null', '2020-01-09 23:54:33'),
+  ('9', '103', '1', '4', '1, 5', '2020-01-10 11:22:59'),
+  ('10', '104', '1', 'null', 'null', '2020-01-11 18:34:49'),
+  ('10', '104', '1', '2, 6', '1, 4', '2020-01-11 18:34:49');
+
+
+DROP TABLE IF EXISTS runner_orders;
+CREATE TABLE runner_orders (
+  "order_id" INTEGER,
+  "runner_id" INTEGER,
+  "pickup_time" VARCHAR(19),
+  "distance" VARCHAR(7),
+  "duration" VARCHAR(10),
+  "cancellation" VARCHAR(23)
+);
+
+INSERT INTO runner_orders
+  ("order_id", "runner_id", "pickup_time", "distance", "duration", "cancellation")
+VALUES
+  ('1', '1', '2020-01-01 18:15:34', '20km', '32 minutes', ''),
+  ('2', '1', '2020-01-01 19:10:54', '20km', '27 minutes', ''),
+  ('3', '1', '2020-01-03 00:12:37', '13.4km', '20 mins', NULL),
+  ('4', '2', '2020-01-04 13:53:03', '23.4', '40', NULL),
+  ('5', '3', '2020-01-08 21:10:57', '10', '15', NULL),
+  ('6', '3', 'null', 'null', 'null', 'Restaurant Cancellation'),
+  ('7', '2', '2020-01-08 21:30:45', '25km', '25mins', 'null'),
+  ('8', '2', '2020-01-10 00:15:02', '23.4 km', '15 minute', 'null'),
+  ('9', '2', 'null', 'null', 'null', 'Customer Cancellation'),
+  ('10', '1', '2020-01-11 18:50:20', '10km', '10minutes', 'null');
+
+
+DROP TABLE IF EXISTS pizza_names;
+CREATE TABLE pizza_names (
+  "pizza_id" INTEGER,
+  "pizza_name" TEXT
+);
+INSERT INTO pizza_names
+  ("pizza_id", "pizza_name")
+VALUES
+  (1, 'Meatlovers'),
+  (2, 'Vegetarian');
+
+
+DROP TABLE IF EXISTS pizza_recipes;
+CREATE TABLE pizza_recipes (
+  "pizza_id" INTEGER,
+  "toppings" TEXT
+);
+INSERT INTO pizza_recipes
+  ("pizza_id", "toppings")
+VALUES
+  (1, '1, 2, 3, 4, 5, 6, 8, 10'),
+  (2, '4, 6, 7, 9, 11, 12');
+
+
+DROP TABLE IF EXISTS pizza_toppings;
+CREATE TABLE pizza_toppings (
+  "topping_id" INTEGER,
+  "topping_name" TEXT
+);
+INSERT INTO pizza_toppings
+  ("topping_id", "topping_name")
+VALUES
+  (1, 'Bacon'),
+  (2, 'BBQ Sauce'),
+  (3, 'Beef'),
+  (4, 'Cheese'),
+  (5, 'Chicken'),
+  (6, 'Mushrooms'),
+  (7, 'Onions'),
+  (8, 'Pepperoni'),
+  (9, 'Peppers'),
+  (10, 'Salami'),
+  (11, 'Tomatoes'),
+  (12, 'Tomato Sauce');
+
+  -----------------------------------------------------------------------------
+
+--  1. How many pizzas were ordered?
+SELECT COUNT(*) AS pizza_order_count
+FROM customer_orders_temp;
+
+--2. How many unique customer orders were made?
+SELECT COUNT(DISTINCT order_id) AS unique_order_count
+FROM customer_orders_temp;
+
+--3. How many successful orders were delivered by each runner?
+SELECT 
+  runner_id, 
+  COUNT(order_id) AS successful_orders
+FROM #runner_orders
+WHERE distance != 0
+GROUP BY runner_id;
+
+.-- How many of each type of pizza was delivered?
+SELECT 
+  p.pizza_name, 
+  COUNT(c.pizza_id) AS delivered_pizza_count
+FROM #customer_orders AS c
+JOIN #runner_orders AS r
+  ON c.order_id = r.order_id
+JOIN pizza_names AS p
+  ON c.pizza_id = p.pizza_id
+WHERE r.distance != 0
+GROUP BY p.pizza_name;
+
+--5. How many Vegetarian and Meatlovers were ordered by each customer?**
+SELECT 
+  c.customer_id, 
+  p.pizza_name, 
+  COUNT(p.pizza_name) AS order_count
+FROM #customer_orders AS c
+JOIN pizza_names AS p
+  ON c.pizza_id= p.pizza_id
+GROUP BY c.customer_id, p.pizza_name
+ORDER BY c.customer_id;
+
+--6. What was the maximum number of pizzas delivered in a single order?
+WITH pizza_count_cte AS
+(
+  SELECT 
+    c.order_id, 
+    COUNT(c.pizza_id) AS pizza_per_order
+  FROM #customer_orders AS c
+  JOIN #runner_orders AS r
+    ON c.order_id = r.order_id
+  WHERE r.distance != 0
+  GROUP BY c.order_id
+)
+
+SELECT 
+  MAX(pizza_per_order) AS pizza_count
+FROM pizza_count_cte;
+
+--7. For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+SELECT 
+  c.customer_id,
+  SUM(
+    CASE WHEN c.exclusions <> ' ' OR c.extras <> ' ' THEN 1
+    ELSE 0
+    END) AS at_least_1_change,
+  SUM(
+    CASE WHEN c.exclusions = ' ' AND c.extras = ' ' THEN 1 
+    ELSE 0
+    END) AS no_change
+FROM #customer_orders AS c
+JOIN #runner_orders AS r
+  ON c.order_id = r.order_id
+WHERE r.distance != 0
+GROUP BY c.customer_id
+ORDER BY c.customer_id;
+
+
+--8. How many pizzas were delivered that had both exclusions and extras?
+SELECT  
+  SUM(
+    CASE WHEN exclusions IS NOT NULL AND extras IS NOT NULL THEN 1
+    ELSE 0
+    END) AS pizza_count_w_exclusions_extras
+FROM #customer_orders AS c
+JOIN #runner_orders AS r
+  ON c.order_id = r.order_id
+WHERE r.distance >= 1 
+  AND exclusions <> ' ' 
+  AND extras <> ' ';
+
+
+  --9. What was the total volume of pizzas ordered for each hour of the day?
+SELECT 
+  DATEPART(HOUR, [order_time]) AS hour_of_day, 
+  COUNT(order_id) AS pizza_count
+FROM #customer_orders
+GROUP BY DATEPART(HOUR, [order_time]);
+
+
+
+--10. What was the volume of orders for each day of the week?
+SELECT 
+  FORMAT(DATEADD(DAY, 2, order_time),'dddd') AS day_of_week, -- add 2 to adjust 1st day of the week as Monday
+  COUNT(order_id) AS total_pizzas_ordered
+FROM #customer_orders
+GROUP BY FORMAT(DATEADD(DAY, 2, order_time),'dddd');
+
+
+
+
+--1. How many runners signed up for each 1 week period? (i.e. week starts 2021-01-01)
+SELECT 
+  DATEPART(WEEK, registration_date) AS registration_week,
+  COUNT(runner_id) AS runner_signup
+FROM runners
+GROUP BY DATEPART(WEEK, registration_date);
+
+
+--2. What was the average time in minutes it took for each runner to arrive at the Pizza Runner HQ to pickup the order?
+WITH time_taken_cte AS
+(
+  SELECT 
+    c.order_id, 
+    c.order_time, 
+    r.pickup_time, 
+    DATEDIFF(MINUTE, c.order_time, r.pickup_time) AS pickup_minutes
+  FROM #customer_orders AS c
+  JOIN #runner_orders AS r
+    ON c.order_id = r.order_id
+  WHERE r.distance != 0
+  GROUP BY c.order_id, c.order_time, r.pickup_time
+)
+
+SELECT 
+  AVG(pickup_minutes) AS avg_pickup_minutes
+FROM time_taken_cte
+WHERE pickup_minutes > 1;
+
+
+
