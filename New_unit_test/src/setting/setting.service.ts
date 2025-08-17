bd13575660acc0945fd404942a1caa1aaf71a93f
budukhcmut <khiembuihk22@gmail.com>
2025-08-17 15:01:06 +0700

    adjust setting

---
+++ New_unit_test/src/setting/setting.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { SettingRepository } from './setting.repository';
+import { SettingUserDto } from './dto/setting.dto';
+
+
+@Injectable()
+export class SettingService {
+  constructor(private readonly settingRepository: SettingRepository) {}
+
+  async getUserSettings(userId: number): Promise<SettingUserDto[]> {
+    return this.settingRepository.getUserSettings(userId);
+  }
+}
