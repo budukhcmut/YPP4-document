4f170001b2759dcdae9946e96e9f7e925a137980
budukhcmut <khiembuihk22@gmail.com>
2025-08-19 14:40:16 +0700

    delete trash folder

--- ggdrive_Unit_test/src/areegate_function/areegate_function.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AreegateFunctionService } from './areegate_function.service';
-import { AreegateFunctionController } from './areegate_function.controller';
-
-@Module({
-  controllers: [AreegateFunctionController],
-  providers: [AreegateFunctionService],
-})
-export class AreegateFunctionModule {}
